fix averaging in vae loss

hw2/src/gan/q1_5.py
@@ -15,18 +15,28 @@ def compute_discriminator_loss(
     loss = E[D(fake_data)] - E[D(real_data)] + lambda * E[(|| grad wrt interpolated_data (D(interpolated_data))|| - 1)^2]
     """
     #first loss term
-    #apply sigmoid
-    discrim_fake_prob = torch.sigmoid(discrim_fake)
-    loss_fake = discrim_fake_prob.mean()
+    loss_fake = discrim_fake.mean()
     
     #second loss term
-    #apply sigmoid
-    discrim_real_prob = torch.sigmoid(discrim_real)
-    loss_real = -discrim_real_prob.mean()
+    loss_real = -discrim_real.mean()
     
     #third loss term
-    loss_grad = torch.autograd.grad(interp, discrim_interp, retain_graph=True)
-    loss_grad = (loss_grad).pow(2).mean()
+    #compute gradients of output w.r.t input
+    loss_grad = torch.autograd.grad(discrim_interp, interp,\
+                                    torch.ones_like(discrim_interp,\
+                                    device="cuda"),
+                                    create_graph=True,
+                                    retain_graph=True,
+                                    only_inputs=True,)[0] #BxCxHxW
+    
+    #reshape gradients
+    loss_grad = loss_grad.view(loss_grad.size(0), -1) #Bx-1
+    
+    #compute norm
+    loss_grad = torch.linalg.norm(loss_grad, dim=-1) #Bx1
+
+    #deviation of norm from 1
+    loss_grad = (loss_grad - 1).pow(2).mean() #1 => scalar
     
     loss = loss_fake + loss_real + lamb*loss_grad
     
@@ -36,12 +46,9 @@ def compute_generator_loss(discrim_fake):
     """
     TODO 1.5.1: Implement WGAN-GP loss for generator.
     loss = - E[D(fake_data)]
-    """
-    #apply sigmoid
-    discrim_fake_prob = torch.sigmoid(discrim_fake)
-    
+    """    
     #compute loss
-    loss = -discrim_fake_prob.mean()
+    loss = -discrim_fake.mean()
     
     return loss
 

hw2/src/gan/train.py
@@ -130,10 +130,10 @@ def train_model(
                 
                 # TODO: 1.5 Compute the interpolated batch and run the discriminator on it.
                 #randomly generate epsilon
-                eps = torch.rand(1)
+                eps = torch.rand(1, device="cuda")
                 
                 #mix generated and real data
-                interp = eps*(gen_out) + (1-eps)*train_batch
+                interp = (eps*(gen_out) + (1-eps)*train_batch.cuda()).requires_grad_(True)
                 
                 #get output from discriminator
                 discrim_interp = disc(interp)

hw2/src/vae/train.py
@@ -26,7 +26,7 @@ def ae_loss(model, x):
     x_pred = model.decoder(z)
     
     #compute loss
-    loss = torch.mean((x_pred - x)**2)
+    loss = torch.sum((x_pred - x)**2)/x.size(0)
     
     return loss, OrderedDict(recon_loss=loss)
 
@@ -54,7 +54,6 @@ def vae_loss(model, x, beta = 1):
     
     #compute reconstruction loss
     recon_loss = torch.sum((x_pred - x)**2)
-    recon_loss = recon_loss/(x.numel())
     
     #compute kl loss
     kl_loss = -0.5*torch.sum(1 + 2*log_std - mu**2 - std**2)
@@ -62,6 +61,9 @@ def vae_loss(model, x, beta = 1):
     
     #compute total loss
     total_loss = recon_loss + beta*kl_loss
+    
+    #average
+    total_loss = total_loss/x.size(0)
     
     # print("total = {}, recon = {}, kl = {}".format(total_loss.cpu().item(), \
     #                                             recon_loss.cpu().item(), \
@@ -129,7 +131,7 @@ def main(log_dir, loss_mode = 'vae', beta_mode = 'constant', num_epochs = 20, ba
     train_loader, val_loader = get_dataloaders()
 
     variational = True if loss_mode == 'vae' else False
-    model = AEModel(variational, latent_size, input_shape = (3, 32, 32)).to("mps") #MY IMPLEMENTATION
+    model = AEModel(variational, latent_size, input_shape = (3, 32, 32)).to("cuda") #MY IMPLEMENTATION
     optimizer = optim.Adam(model.parameters(), lr=lr)
 
     vis_x = next(iter(val_loader))[0][:36]