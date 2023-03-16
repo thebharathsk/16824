8eb75966da93880a90d2fbd79a1288280a1f7a49
bharath somayajula <thebharathsk@gmail.com>
2023-03-16 16:54:56 -0400

    write code for q3.1

--- hw2/src/diffusion/model.py
+++ hw2/src/diffusion/model.py
@@ -30,24 +30,29 @@ class DiffusionModel(nn.Module):
 
         alphas = 1. - self.betas
         # TODO 3.1: compute the cumulative products for current and previous timesteps
-        self.alphas_cumprod = ...
-        self.alphas_cumprod_prev =  ...
+        #MY IMPLEMENTATION
+        self.alphas_cumprod = torch.cumprod(alphas, 0)
+        print(self.alphas_cumprod.shape)
+        self.alphas_cumprod_prev =  torch.roll(self.alphas_cumprod, 1)
 
         # TODO 3.1: pre-compute values needed for forward process
+        #MY IMPLEMENTATION
         # This is the coefficient of x_t when predicting x_0
-        self.x_0_pred_coef_1 = ...
+        self.x_0_pred_coef_1 = 1/torch.sqrt(self.alphas_cumprod)
         # This is the coefficient of pred_noise when predicting x_0
-        self.x_0_pred_coef_2 = ...
+        self.x_0_pred_coef_2 = -torch.sqrt(1-self.alphas_cumprod/self.alphas_cumprod)
 
         # TODO 3.1: compute the coefficients for the mean
+        #MY IMPLEMENTATION
         # This is coefficient of x_0 in the DDPM section
-        self.posterior_mean_coef1 = ...
+        self.posterior_mean_coef1 = (torch.sqrt(self.alphas_cumprod_prev)/(1-self.alphas_cumprod))*self.betas
         # This is coefficient of x_t in the DDPM section
-        self.posterior_mean_coef2 = ...
+        self.posterior_mean_coef2 = torch.sqrt(self.alphas_cumprod)*((1-self.alphas_cumprod_prev)/(1-self.alphas_cumprod))
 
         # TODO 3.1: compute posterior variance
         # calculations for posterior q(x_{t-1} | x_t, x_0) in DDPM
-        self.posterior_variance = ...
+        #MY IMPLEMENTATION
+        self.posterior_variance = (((1-self.alphas_cumprod_prev)/(1-self.alphas_cumprod))*self.betas).pow(2)
         self.posterior_log_variance_clipped = torch.log(
             self.posterior_variance.clamp(min =1e-20))
 
@@ -62,6 +67,16 @@ class DiffusionModel(nn.Module):
         # TODO 3.1: Compute the posterior mean and variance for x_{t-1}
         # using the coefficients, x_t, and x_0
         # hint: can use extract function from utils.py
+        #MY IMPLEMENTATION
+        #mean
+        posterior_mean = self.posterior_mean_coef1[t]*x_0 + self.posterior_mean_coef2[t]*x_t
+        
+        #variance
+        posterior_variance = self.posterior_variance[t]
+        
+        #log variance
+        posterior_log_variance_clipped = self.posterior_log_variance_clipped[t]
+        
         return posterior_mean, posterior_variance, posterior_log_variance_clipped
 
     def model_predictions(self, x_t, t):
@@ -69,6 +84,17 @@ class DiffusionModel(nn.Module):
         # to predict the additive noise, use the denoising model.
         # Hint: You can use extract function from utils.py.
         # clamp x_0 to [-1, 1]
+        #MY IMPLEMENTATION
+        #predict noise
+        pred_noise = self.model(x_t, t)
+        
+        #use extract function
+        pred_noise = extract(pred_noise, t, x_t.shape)
+        
+        #estimate x_0
+        x_0 = self.x_0_pred_coef_1[t]*x_t + \
+                self.x_0_pred_coef_2[t]*pred_noise
+        
         return (pred_noise, x_0)
 
     @torch.no_grad()
@@ -76,6 +102,15 @@ class DiffusionModel(nn.Module):
         # TODO 3.1: given x at timestep t, predict the denoised image at x_{t-1}.
         # also return the predicted starting image.
         # Hint: To do this, you will need a predicted x_0. Which function can do this for you?
+        #first estimate x_0
+        pred_noise, x_0 = self.model_predictions(x, t)
+        
+        #estimate mu and variance
+        mu, var, _ = self.get_posterior_parameters(x_0, x, t)
+        
+        #estimate x_t-1
+        pred_img = mu + var*pred_noise
+        
         return pred_img, x_0
 
     @torch.no_grad()
